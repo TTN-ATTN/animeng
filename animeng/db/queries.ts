617b7e19e1436b84e69b49aea7c27d42ce6fda26
Tran Trung Nhan <23521090@gm.uit.edu.vn>
2025-04-02 11:17:44 +0700

    fetching courses' data

--- animeng/db/queries.ts
+++ animeng/db/queries.ts
@@ -2,26 +2,26 @@
     Backend: Định nghĩa các truy vấn dữ liệu từ cơ sở dữ liệu
 */
 
-import {cache} from "react";
+import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
-import { courses, userProgress } from "./schema";
+import { courses, userProgress, units } from "./schema";
 import { eq } from "drizzle-orm";
 
 // cache được dùng để lưu trữ dữ liệu tạm thời, giúp giảm thiểu việc truyền Props 
 export const getCourses = cache(
-    async() => {
+    async () => {
         const data = await db.query.courses.findMany();
         return data;
     }
 )
 
 export const getUserProgress = cache(
-    async() => {
+    async () => {
         const { userId } = await auth();
-        if(!userId) return null;    
+        if (!userId) return null;
         const data = await db.query.userProgress.findFirst({
-            where: eq(userProgress.userId, userId), 
+            where: eq(userProgress.userId, userId),
             with: {
                 activeCourse: true
             }
@@ -32,10 +32,42 @@ export const getUserProgress = cache(
 );
 
 export const getCourseById = cache(
-    async(courseId: number) => {
+    async (courseId: number) => {
         const data = await db.query.courses.findFirst({
             where: eq(courses.id, courseId)
         });
         return data;
-    }   
-)
+    }
+);
+
+export const getUnits = cache(async () => {
+    const userProgress = await getUserProgress();
+    if (!userProgress?.activeCourseId) return [];
+
+    const temp_data = await db.query.units.findMany({
+        where: eq(units.courseId, userProgress.activeCourseId),
+        with: {lessons: {with: {challenges: {with: {challProgress: true}}}}},
+    });
+
+    // Map unit ở trong temp_data 
+    const data = temp_data.map((unit) => {
+        // Map lesson ở trong unit
+        const completedLessons = unit.lessons.map((lesson)=>{
+            // Kiểm tra xem tất cả các thử thách trong bài học đã hoàn thành chưa
+            const completedChallenges = lesson.challenges.every((challenge) => {
+                return challenge.challProgress 
+                && challenge.challProgress.length>0 
+                && challenge.challProgress.every((cp) => {return cp.completed});
+            });
+
+            // Spread operator được sử dụng để sao chép tất cả các thuộc tính của đối tượng lesson vào một đối tượng mới, và thêm thuộc tính completed với giá trị là completedChallenges
+            return {...lesson, completed: completedChallenges};
+        });
+
+        // Thêm thuộc tính lessons vào đối tượng unit, với giá trị là completedLessons
+        return {...unit, lessons: completedLessons};
+    });
+
+    return data;
+});
+
--- animeng/src/app/(main)/learning/page.tsx
+++ animeng/src/app/(main)/learning/page.tsx
@@ -2,12 +2,13 @@ import { FeedWrapper } from "@/components/ui/feed-wrapper";
 import { StickyWrapper } from "@/components/ui/sticky-wrapper";
 import { Header2 } from "../header";
 import { UserProgress } from "@/components/ui/user-progress";
-import { getUserProgress } from "../../../../db/queries";
+import { getUnits, getUserProgress } from "../../../../db/queries";
 import { redirect } from "next/navigation";
 
 
 export default async function LearningPage(){
     const userProgress = await getUserProgress();
+    const units = await getUnits();
 
     // Nếu không có khóa học đang học thì redirect về trang danh sách khóa học
     if(!userProgress || !userProgress.activeCourse){
@@ -23,7 +24,16 @@ export default async function LearningPage(){
                 hasSubscription={false}
             />
             </StickyWrapper>
-            <FeedWrapper><Header2 title={userProgress.activeCourse.title}/></FeedWrapper>
+            
+            <FeedWrapper><Header2 title={userProgress.activeCourse.title}/>
+            {units.map((unit) => {
+                return(
+                    <div key={unit.id} className="mb-10">
+                        {JSON.stringify(unit)}
+                    </div>
+                )
+            })}
+            </FeedWrapper>
         </div>
     );
 }

1c149f6406d5e3b4d8f01ed699155bdfeb025286
anotherme13 <lovelyboy@DESKTOP-110ET22.>
2025-04-23 18:00:23 +0700

    delete a redundant folder

--- animeng/src/app/(main)/chat/page.tsx
+++
@@ -1,9 +0,0 @@
-const RankingPage = async () => {
-    return (
-        <div className="h-full max-w-[912px] mx-auto px-3">
-            <h1 className="text-2xl font-bold text-neutral-700 mt-12">Trò chuyện cùng với gái alimi</h1>
-        </div>
-    )
-}
-
-export default RankingPage;
--- animeng/src/app/(main)/quests/page.tsx
+++
@@ -1,9 +0,0 @@
-const QuestPage = async () => {
-    return (
-        <div className="h-full max-w-[912px] mx-auto px-3">
-            <h1 className="text-2xl font-bold text-neutral-700 mt-12">Nhiệm vụ trong ngày</h1>
-        </div>
-    )
-}
-
-export default QuestPage;

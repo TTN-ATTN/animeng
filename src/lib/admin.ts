122fefd3fbe98abd1f31a2d983b71a37670186e8
PiRin <146969222+2pi4rin7@users.noreply.github.com>
2025-05-25 19:58:58 +0700

    Add(lib/admin.ts) add new user admin

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs/server"
 
 const whitelist = [
     "user_...pCqHKs3E", // Your user ID here.
+    "user_2w7xQcYyBfgl0NRVg4w0ZrcWLjB"
 ]
 
 export const getIsAdmin = async () => {

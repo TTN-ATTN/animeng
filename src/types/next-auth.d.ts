a66ab584c45d42b9c31b2bb4d6cc6764b3904ca0
PiRin <binhnhan.pahn@gmail.com>
2025-06-06 23:03:36 +0700

    Add new dependences package

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,23 +1,22 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 
 declare module "next-auth" {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
   interface Session {
     user: {
-      /** The user's id. */
-      id: string
-      name?: string | null
-      email?: string | null
-      image?: string | null
-    }
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+
+  interface User {
+    id: string;
   }
 }
 
 declare module "next-auth/jwt" {
-  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
   interface JWT {
-    id: string
+    id: string;
   }
 }

e9e5e973541d1313c5ae64769fde8249f2d9aa19
PiRin <binhnhan.pahn@gmail.com>
2025-06-06 23:01:40 +0700

    add reset-password page

---
+++ src/app/reset-password/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import ResetForm from "@/components/client/ResetForm";
+import { useSearchParams } from 'next/navigation';
+
+
+export default function ResetPasswordPage() {
+    const searchParams = useSearchParams();
+    const userId = searchParams.get('userId');
+    const token = searchParams.get('token');
+    if (!userId || !token) {
+        return (
+            <div className="min-h-screen flex items-center justify-center bg-[#F7F7F7]">
+                <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
+                    <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
+                        404 - Page Not Found
+                        </h1>
+                        </div>
+                        </div>
+                        )
+    }
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-[#F7F7F7]">
+            <ResetForm userId={userId} token={token} />
+        </div>
+    );
+
+}
